16032449e1f8ee999814bc7c45cb76edea78d4b8
kstratis <stratis.konstantinos@gmail.com>
2016-03-18 16:25:50 +0200

    Weather app completed successfully

---
+++ app/containers/DetailContainer.js
@@ -0,0 +1,16 @@
+var React = require('react');
+var Detail = require('../components/Detail');
+
+var DetailContainer = React.createClass({
+  render: function() {
+    // console.log(this.props.location.state.weather)
+    return (
+      <Detail
+        weather={this.props.location.state.weather}
+        city={this.props.routeParams.city} />
+    );
+  }
+
+});
+
+module.exports = DetailContainer;
